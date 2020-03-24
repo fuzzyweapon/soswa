153ab14846ab04017e7be9d453b14ac17e3a4ac1
julia <fuzzyweapon@gmail.com>
2020-03-24 16:19:05 -0700

    Display nickname in glance

--- plugins/describe/char_desc_template_fields.rb
+++ plugins/describe/char_desc_template_fields.rb
@@ -5,7 +5,7 @@ module AresMUSH
       char.rank
     end
     
-    # Fullname with rank and callsign, if set.  (e.g. Commander William "Husker" Adama)
+    # Fullname with rank and nickname, if set.  (e.g. Commander William "Husker" Adama)
     def military_name(char)
       Ranks.military_name(char)
     end
