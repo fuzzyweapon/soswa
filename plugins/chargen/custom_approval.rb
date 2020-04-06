3be8cc8a62c182cc81a64db2773265df38d56206
fuzzyweapon <fuzzyweapon@gmail.com>
2020-04-06 12:30:52 -0700

    Add automation for channels and roles based on Department

--- plugins/chargen/custom_approval.rb
+++ plugins/chargen/custom_approval.rb
@@ -1,9 +1,9 @@
 module AresMUSH
   module Chargen
     def self.custom_approval(char)
-            
+
       # If you don't want to have any custom approval steps, just leave this blank.
-      
+
       # Otherwise, do what you need to do.  Here's an example of how to add
       # someone to a role based on their faction:
       #
@@ -15,6 +15,23 @@ module AresMUSH
       #  end
       #
       # See https://www.aresmush.com/tutorials/config/chargen.html for details.
+      department = char.group("Department")
+      if department == "Command"
+        department = "Tactical"
+      end
+      department_hyphen = department.sub(" ", "-")
+
+      role = Role.find_one_by_name(department_hyphen)
+      channel = Channel.find_one_by_name(department)
+
+      if role
+        Roles.add_role(char, role.name)
+      end
+
+      if channel && Channels.can_join_channel?(char, channel)
+        Channels.join_channel(channel, char)
+      end
+
     end
   end
 end
